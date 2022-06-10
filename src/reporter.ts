6b49df4f3104d5e4ce45d28b58757c751c6b78c9
Eike Lurz <eike.lurz@sportalliance.com>
2022-06-10 22:32:39 +0200

    Use github actions core

--- src/reporter.ts
+++ src/reporter.ts
@@ -1,25 +1,18 @@
 import type { Awaitable, File, Reporter } from 'vitest'
-import { buildFileAppender } from './file-appender'
-import type { FileAppender, Table } from './file-appender'
-import { buildRowFromFile } from './table'
+import { summary } from '@actions/core'
+import { buildSummaryTable } from './summary-table'
 
 class GithubActionsReporter implements Reporter {
-  private fileAppender: FileAppender
-
-  constructor() {
-    const stepSummaryFile = process.env.GITHUB_STEP_SUMMARY
-    if (!stepSummaryFile) throw Error('GITHUB_STEP_SUMMARY needs to be set')
-    this.fileAppender = buildFileAppender(stepSummaryFile)
-  }
-
   onFinished(files?: File[], errors?: unknown[]): Awaitable<void> {
-    this.fileAppender.appendLine('# Test Results')
-    const table: Table = {
-      headers: ['File', 'Result', 'Skipped', 'Duration'],
-      rows: files?.flatMap(buildRowFromFile) ?? [],
-    }
-    console.log(table)
-    this.fileAppender.appendTable(table)
+    summary.addHeading('Test Results', 1)
+    const summaryTable = buildSummaryTable([
+      'File',
+      'Result',
+      'Skipped',
+      'Duration',
+    ])
+    files?.map(summaryTable.addFile)
+    summary.addTable(summaryTable.getRows())
 
     return Promise.resolve()
   }
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig, configDefaults } from 'vitest/config'
+import { GithubActionsReporter } from './src'
 
 export default defineConfig({
   test: {
     ...configDefaults,
     clearMocks: true,
+    reporters: process.env.CI
+      ? [new GithubActionsReporter()]
+      : configDefaults.reporters,
   },
 })
